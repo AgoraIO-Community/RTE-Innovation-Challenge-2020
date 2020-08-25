7904dee569dfb8319d5459b1032fce09e6484503
Lofter <xiaosheng0601@qq.com>
2020-08-25 15:29:46 +0800

    commit
    commit

---
+++ SDKChallengeProject/GameParty-Node/redis.js
@@ -0,0 +1,20 @@
+var Redis = require('ioredis');
+
+function createClient() {
+  // 本地环境下此环境变量为 undefined, 会链接到默认的 127.0.0.1:6379
+  var redisClient = new Redis(process.env.REDIS_URL_game_type_cache);
+
+  redisClient.on('error', function(err) {
+    if (process.env.NODE_ENV !== 'production' && process.env.REDIS_URL_test) {
+      console.error('redis err: %s', err);
+    }
+    console.log('cacheport >>>> ',process.env.REDIS_URL_test);
+  });
+
+  return redisClient;
+}
+
+module.exports = {
+  redisClient: createClient(),
+  createClient: createClient
+};
