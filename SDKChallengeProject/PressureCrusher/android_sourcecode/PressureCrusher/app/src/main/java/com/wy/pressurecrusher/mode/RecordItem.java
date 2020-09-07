d04db38acff4a915007850f6c6c14372739ad107
sunz10 <35555989+sunz10@users.noreply.github.com>
2020-09-07 15:42:08 +0800

    Revert "my project: PressureCrusher"

--- SDKChallengeProject/PressureCrusher/android_sourcecode/PressureCrusher/app/src/main/java/com/wy/pressurecrusher/mode/RecordItem.java
+++
@@ -1,8 +0,0 @@
-package com.wy.pressurecrusher.mode;
-
-public class RecordItem {
-    public String id;
-    public String name;//名称
-    public String filePath;//本地文件路径
-    public String length;//时长
-}
