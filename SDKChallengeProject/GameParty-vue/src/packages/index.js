49db6cb83db25cc72f0fc625e6acb9fd517b55d8
sunz10 <35555989+sunz10@users.noreply.github.com>
2020-09-07 14:02:05 +0800

    Revert "【奔腾江河】G游语音-找基友上G游"

--- SDKChallengeProject/GameParty-vue/src/packages/index.js
+++
@@ -1,8 +0,0 @@
-import HxToast from './toast/index.js'
-
-const install = function(Vue) {
-  Vue.component(HxToast.name, HxToast)
-
-  Vue.prototype.$hx_toast = HxToast.instalToast
-}
-export default install
